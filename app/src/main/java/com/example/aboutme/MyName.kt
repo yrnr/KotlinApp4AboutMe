b042ad9253986c4131a1cb3e4d5ca7f5d020e40e
yrnr <iamramuhere@gmail.com>
2020-06-24 17:20:23 +0530

    databinding class MyName is put to use in views and activity_main

---
+++ app/src/main/java/com/example/aboutme/MyName.kt
@@ -0,0 +1,8 @@
+package com.example.aboutme
+
+data class MyName(var name: String = "", var nickname: String = "")
+//data class MyName(var name: String = "", var nickname: String = "")
+//{
+//    var self.name: String = "Ramanadha Reddy"
+//    var self.nickname: String = ""
+//}
